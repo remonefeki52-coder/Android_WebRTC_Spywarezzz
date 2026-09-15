41c04a992c75be13501dc0edc8f279cbde970d82
remonefeki52-coder <remonefeki52@gmail.com>
2026-09-15 19:37:46 +0300

    Update server.js

--- Android-WebRTC-Spyware-Server/server.js
+++ Android-WebRTC-Spyware-Server/server.js
@@ -103,7 +103,11 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get('*', (req, res) => {
+app.get('*', (req, res, next) => {
+  // Skip Socket.IO paths - let Socket.IO handle them
+  if (req.path.startsWith('/socket.io')) {
+    return next();
+  }
   const indexPath = path.join(publicPath, 'index.html');
   if (fs.existsSync(indexPath)) {
     console.log(`Serving index.html for ${req.url}`);
